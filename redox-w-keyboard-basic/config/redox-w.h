7097e27f7605f21d81ba494ca2dc0a2ff8e4be7b
Nikos Nikoleris <nikos.nikoleris@gmail.com>
2021-02-24 21:55:05 +0000

    Remap pin mappings to simplify handling and avoid shifts
    By changing the order in which we read pins we can simplify how we
    convert this to the keyboard matrix and avoid a number of shifts.
    
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@gmail.com>

--- redox-w-keyboard-basic/config/redox-w.h
+++ redox-w-keyboard-basic/config/redox-w.h
@@ -9,13 +9,14 @@
 
 #define PIPE_NUMBER 0
 
-#define C01 10
-#define C02 9
-#define C03 7
+#define C01 3
+#define C02 4
+#define C03 5
 #define C04 6
-#define C05 5
-#define C06 4
-#define C07 3
+#define C05 7
+#define C06 9
+#define C07 10
+
 #define R01 19
 #define R02 18
 #define R03 17
@@ -28,13 +29,14 @@
 
 #define PIPE_NUMBER 1
 
-#define C01 6
-#define C02 5
-#define C03 4
+#define C01 30
+#define C02 0
+#define C03 2
 #define C04 3
-#define C05 2
-#define C06 0
-#define C07 30
+#define C05 4
+#define C06 5
+#define C07 6
+
 #define R01 21
 #define R02 22
 #define R03 23
